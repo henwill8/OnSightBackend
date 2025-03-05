f0498ce4d360aee3c3c9db0fc40c09fba0118e61
henwill8 <henwill8@yahoo.com>
2025-03-04 20:25:08 -0700

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Define your API routes here
+app.get('/api/example', (req: Request, res: Response) => {
+    res.json({ message: 'This is an example route' });
+});
+
+// Root route
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the root page!');
+});
+
+// Catch-all for any unmatched routes and redirect to root
+app.use((req: Request, res: Response) => {
+    res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
